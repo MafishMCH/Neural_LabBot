1a0363a446f07ed83b0d4711ec79819af261b76c
Jakub Matyszczak <mafish@pop-os.localdomain>
2019-06-13 15:43:59 +0200

    removed unused files

--- sigmoid.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-"""Sigmoid Function"""
-from numpy import power, e
-
-def sigmoid(x_value):
-    """Return the sigmoid value"""
-    print(x_value)
-    return 1.0/(1.0 + power(e, -x_value))
